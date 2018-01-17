df93fd8c31921b9811438486ec09cdfa74673b87
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-01-17 08:18:34 -0500

    Updated from Brython Server: 1/17/2018 8:18:35 AM

--- inputDemo.py
+++ inputDemo.py
@@ -1,1 +1,10 @@
+#Julia Golder
+#1/17/18
+#inputdemo.py - how to use input
+
+name = input('What is your name')
+print('Hello', name)
+age = int(input('How old are you?' ))
+print('You are', age+1, 'years old')
+
 
