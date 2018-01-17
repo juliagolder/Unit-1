e5e5906fbded08872065b39af9f87887dc5d866b
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-01-17 08:25:46 -0500

    Updated from Brython Server: 1/17/2018 8:25:47 AM

--- face.py
+++ face.py
@@ -1,1 +1,10 @@
+#Julia Golder
+#1/17/18
+#face.py - how to program a face
 
+print('       xxxxx')
+print('     x       x')
+print(' ((     o o     ))')
+print('   |     V     | ')
+print('   |    ===    | ')
+print('       _____')
