bddb93d89f3214a548717926ca78943860586329
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-01-22 08:16:41 -0500

    Update additionGameDemo.py

--- additionGameDemo.py
+++ additionGameDemo.py
@@ -1,1 +1,11 @@
+#Julia Golder
+#1/22/18
+#additionGameDemo.py - how to use random numbers
 
+from random import randint
+
+num1= randint(-10,10)
+num2= randint(-10,10)
+print(num1, '+', num2)
+ans = int(input(str(num1)+ '+' + str(num2) + '='))
+print(ans == num1 + num2)
