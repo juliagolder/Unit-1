421e87bd96035b8c86c88b04a3b1494d8392f216
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-01-26 08:15:40 -0500

    Updated from Brython Server: 1/26/2018 8:15:40 AM

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,10 @@
+#julia golder
+#1/26/18
+#quiz1.py - first quiz
 
+print('Hello)
+name = input('What is your name?')
+number = input('What is your favorite number?')
+print(name*number)
+number2 = input('What is your second favorite number?')
+lucky = print(number*number2)
