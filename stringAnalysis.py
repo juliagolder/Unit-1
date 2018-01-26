d53fe491dcb6e359b7b4663620cc484fb8c5b0e8
juliagolder <35492759+juliagolder@users.noreply.github.com>
2018-01-26 08:01:56 -0500

    Update stringAnalysis.py

--- stringAnalysis.py
+++ stringAnalysis.py
@@ -3,14 +3,12 @@
 #stringAnalysis.py - 
 
 
-text = input('Enter a sentence')
-print
+text = input('Enter a sentence').lower()
 spaces = text.count(' ')
 print('Your sentence has', spaces+1, 'words', (len(text)), 'characters', len(text)-spaces , 'letters')
 character = input('What character would you like to search for?')
 number_of_characters = text.count(character)
 print('Your sentence has', number_of_characters , 'of the charcter', character) 
-character.lower()
 words = input('What word would you like to search for?')
 number_of_words = text.count(words)
 print('Your sentence has', number_of_words , 'of the word', words) 
